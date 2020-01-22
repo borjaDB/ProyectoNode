b620c2e41cfd51227958a4e8347a56825f1cfabb
borja981 <33958932+borja981@users.noreply.github.com>
2020-01-22 13:05:15 +0100

    funciones
    CCAA y cadena con express.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 var express = require("express");
 var app = express();
-
+//Las peticiones entran por este metodo a modo controlador
 app.get("/", function(req, res) {
-  res.send("Hello World!");
+  //1. Se pinta en consola la lista
+  crearComAutonomas();
+  //2. Se crea una respueta llamando al metodo crear cadena
+  var crearCadena = crearCadenaFunction();
+  //solo una respuesta por llamada
+  res.send(crearCadena);
 });
+//1
+function crearCadenaFunction() {
+  var hr = "<hr>";
+  return "<strong>Esto es una prueba de texto<strong>" + hr;
+}
+//2
+function crearComAutonomas() {
+  var myData = [];
+  myData.push("Galicia", "Madrid", "Asturias");
+
+  for (var item in myData) {
+    console.log(myData[item]);
+  }
+}
 
 app.listen(1337, function() {
   console.log("Example app listening on port 3000!");
